f129e8cc5e49f1b109e857455e0c405bb026dc85
Thomas <tomytomtom678@gmail.com>
2018-05-01 21:12:50 +0800

    PHP task five web

---
+++ PHP Task 5/Car.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 1/05/2018
+ * Time: 9:02 PM
+ */
+
+namespace Car;
+
+
+class Car
+{
+
+}
---
+++ PHP Task 5/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 1/05/2018
+ * Time: 8:03 PM
+ */
+?>
